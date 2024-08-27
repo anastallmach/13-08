ca00aa3e94ecbc86e32f9e7499eb301ce62c8a04
anastallmach <159021305+anastallmach@users.noreply.github.com>
2024-08-27 10:44:42 -0300

    js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-const caixaPrincipal = document.querySelector(".caixaPrincipal");
-const caixaPrincipal = document.querySelector(".caixaPerguntas");
-const caixaP
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
